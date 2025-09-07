7837b7d610922848fe43204b16324fdbebd0681a
Mohid Meer <mohidfauji@gmail.com>
2025-09-07 16:52:13 +0500

    updated

---
+++ data.js
@@ -0,0 +1,13 @@
+export const tourPackages = [
+    {
+        name: "Neelum Valley  Escape",
+        price: 35000,
+        image: "https://cdn.pixabay.com/photo/2019/07/17/09/49/mountains-4343542_1280.jpg",
+        duration: {
+            days: 3,
+            nights: 2,
+        },
+        placesToVisit: ["Kel Village", "Sharda", "Arang Kel", "Keran"],
+        description: "Experience the breathtaking beauty of Neelum Valley, known for its lush green forests, crystal-clear rivers, and peaceful mountain villages. This tour offers a perfect blend of adventure and relaxation, with visits to Kel Village’s serene landscapes, the historical ruins at Sharda, and the mesmerizing views from Arang Kel. Ideal for travelers who want to connect deeply with nature and local culture."
+    },
+];
